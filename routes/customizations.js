162891106a438c7bec4d8b3c11bc1fcbae20747f
EC2 Default User <ec2-user@ip-172-31-74-143.ec2.internal>
2018-03-10 17:44:17 +0000

    Base app

---
+++ routes/customizations.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET customizations listing. */
+router.get('/', function(req, res, next) {
+  res.render('customizations', {title: 'Customizations'});
+});
+
+/* GET skins route */
+router.get('/skins', function(req, res, next) {
+  res.send('This is the /customizations/skins page');
+});
+
+/* GET taunts route */
+router.get('/taunts', function(req, res, next) {
+  res.send('This is the /customizations/taunts page');
+});
+
+/* GET stickers route */
+router.get('/stickers', function(req, res, next) {
+  res.send('This is the /customizations/stickers page');
+});
+
+/* GET pickaxes route */
+router.get('/pickaxes', function(req, res, next) {
+  res.send('This is the /customizations/pickaxes page');
+});
+
+/* GET gliders route */
+router.get('/gliders', function(req, res, next) {
+  res.send('This is the /customizations/gliders page');
+});
+
+/* GET backpacks route */
+router.get('/backpacks', function(req, res, next) {
+  res.send('This is the /customizations/backpacks page');
+});
+
+module.exports = router;
---
+++ routes/loot.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET loot listing. */
+router.get('/', function(req, res, next) {
+  res.render('loot', { title: 'Loot Stats' });
+});
+
+/* GET weapons route */
+router.get('/weapons', function(req, res, next) {
+  res.send('This is the /loot/weapons page');
+});
+
+/* GET weapons/:weaponName listing. */
+router.get('/weapons/:weaponName', function(req, res, next) {
+  console.log(req.params);
+  res.send('This is the loot/weapons/' + req.params.weaponName + ' page!');
+});
+
+
+
+
+
+
+
+
+/* GET healing route */
+router.get('/healing', function(req, res, next) {
+  res.send('This is the /loot/healing page');
+});
+
+/* GET specials route */
+router.get('/specials', function(req, res, next) {
+  res.send('This is the /loot/specials page');
+});
+
+/* GET materials route */
+router.get('/materials', function(req, res, next) {
+  res.send('This is the /loot/materials page');
+});
+
+module.exports = router;
