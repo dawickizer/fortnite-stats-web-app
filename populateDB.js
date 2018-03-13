d03f6fa47e904d567446534cd0253958d4a7ca30
EC2 Default User <ec2-user@ip-172-31-74-143.ec2.internal>
2018-03-13 16:57:26 +0000

    (1) Added weapon data generation to populateDB.js (2) /loot/weapons route now renders weapons.ejs with weapon data from database

--- populateDB.js
+++ populateDB.js
@@ -23,7 +23,7 @@ mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection 
 
 // Test adding to DB
 // Testing.create({
-//     name: "Burst",
+//     name: "Test",
 //     number: -1
 //   },
 //   function (err, small) {
@@ -31,8 +31,7 @@ mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection 
 //     // saved!
 // });
 
-
-// Create weapon document in Database
+// Create common assault-rifle-burst weapon document in Database
 Weapon.create({
     image: "/public/images/weapons/assault-rifle-burst.png",
     name: "assault-rifle-burst",
@@ -85,3 +84,275 @@ Weapon.create({
     if (err) console.log(err);
     // saved!
 });
+
+// Create uncommon assault-rifle-burst weapon document in Database
+Weapon.create({
+    image: "/public/images/weapons/assault-rifle-burst.png",
+    name: "assault-rifle-burst",
+    type: "assault-rifle",
+    tier: "uncommon",
+    closeRange: 1,
+    midrange: 1,
+    longRange: 1,
+    maxRange: 250000,
+    damagePerSecond: 50.8,
+    bodyDamageClose: 29,
+    headDamageClose: 72.5,
+    bodyDamageMid: 29,
+    headDamageMid: 72.5,
+    bodyDamageLong: 29,
+    headDamageLong: 72.5,
+    bodyDamageMax: 29,
+    headDamageMax: 72.5,
+    impactDamageClose: 42,
+    impactDamageMid: 42,
+    impactDamageLong: 42,
+    impactDamageMax: 42,
+    environmentalDamageClose: 30,
+    environmentalDamageMid: 30,
+    environmentalDamageLong: 30,
+    environmentalDamageMax: 30,
+    baseSpread: .175,
+    sprintSpread: 1.5,
+    fallingJumpingSpread: 1.25,
+    adsSpread: .65,
+    standingSpread: .55,
+    crouchingSpread: .75,
+    horizontalRecoil: 0.25,
+    horizontalRecoilGamepad: 1,
+    verticalRecoil: 1.8375,
+    verticalRecoilGamepad: .5,
+    angleMax: 25,
+    angleMin: -25,
+    adsRecoil: .5,
+    interpSpeed: 12,
+    interpSpeedRecovery: 9,
+    fireRate: 1.75,
+    reloadTime: 2.7,
+    magazineSize: 30,
+    floorLootChance: .258,
+    chestLootChance: .258,
+    supplyDropLootChance: 0
+  },
+  function (err, small) {
+    if (err) console.log(err);
+    // saved!
+});
+
+// Create rare assault-rifle-burst weapon document in Database
+Weapon.create({
+    image: "/public/images/weapons/assault-rifle-burst.png",
+    name: "assault-rifle-burst",
+    type: "assault-rifle",
+    tier: "rare",
+    closeRange: 1,
+    midrange: 1,
+    longRange: 1,
+    maxRange: 250000,
+    damagePerSecond: 52.5,
+    bodyDamageClose: 30,
+    headDamageClose: 75.0,
+    bodyDamageMid: 30,
+    headDamageMid: 75.0,
+    bodyDamageLong: 30,
+    headDamageLong: 75.0,
+    bodyDamageMax: 30,
+    headDamageMax: 75.0,
+    impactDamageClose: 44,
+    impactDamageMid: 44,
+    impactDamageLong: 44,
+    impactDamageMax: 44,
+    environmentalDamageClose: 30,
+    environmentalDamageMid: 30,
+    environmentalDamageLong: 30,
+    environmentalDamageMax: 30,
+    baseSpread: .175,
+    sprintSpread: 1.5,
+    fallingJumpingSpread: 1.25,
+    adsSpread: .65,
+    standingSpread: .55,
+    crouchingSpread: .75,
+    horizontalRecoil: 0.25,
+    horizontalRecoilGamepad: 1,
+    verticalRecoil: 1.75,
+    verticalRecoilGamepad: .5,
+    angleMax: 25,
+    angleMin: -25,
+    adsRecoil: .5,
+    interpSpeed: 12,
+    interpSpeedRecovery: 9,
+    fireRate: 1.75,
+    reloadTime: 2.6,
+    magazineSize: 30,
+    floorLootChance: .097,
+    chestLootChance: .097,
+    supplyDropLootChance: 0
+  },
+  function (err, small) {
+    if (err) console.log(err);
+    // saved!
+});
+
+// Create common m4 weapon document in Database
+Weapon.create({
+    image: "/public/images/weapons/assault-rifle-m4.png",
+    name: "assault-rifle-m4",
+    type: "assault-rifle",
+    tier: "common",
+    closeRange: 1,
+    midrange: 1,
+    longRange: 1,
+    maxRange: 250000,
+    damagePerSecond: 165,
+    bodyDamageClose: 30,
+    headDamageClose: 60,
+    bodyDamageMid: 30,
+    headDamageMid: 60,
+    bodyDamageLong: 30,
+    headDamageLong: 60,
+    bodyDamageMax: 30,
+    headDamageMax: 60,
+    impactDamageClose: 25,
+    impactDamageMid: 25,
+    impactDamageLong: 25,
+    impactDamageMax: 25,
+    environmentalDamageClose: 33,
+    environmentalDamageMid: 33,
+    environmentalDamageLong: 33,
+    environmentalDamageMax: 33,
+    baseSpread: .15,
+    sprintSpread: 1.5,
+    fallingJumpingSpread: 1.25,
+    adsSpread: .6,
+    standingSpread: .55,
+    crouchingSpread: .8,
+    horizontalRecoil: 0.25,
+    horizontalRecoilGamepad: 1,
+    verticalRecoil: 3.5,
+    verticalRecoilGamepad: .5,
+    angleMax: 25,
+    angleMin: -25,
+    adsRecoil: .5,
+    interpSpeed: 12,
+    interpSpeedRecovery: 6,
+    fireRate: 5.5,
+    reloadTime: 2.3,
+    magazineSize: 30,
+    floorLootChance: .518,
+    chestLootChance: .518,
+    supplyDropLootChance: 0
+  },
+  function (err, small) {
+    if (err) console.log(err);
+    // saved!
+});
+
+// Create uncommon m4 weapon document in Database
+Weapon.create({
+    image: "/public/images/weapons/assault-rifle-m4.png",
+    name: "assault-rifle-m4",
+    type: "assault-rifle",
+    tier: "uncommon",
+    closeRange: 1,
+    midrange: 1,
+    longRange: 1,
+    maxRange: 250000,
+    damagePerSecond: 170.5,
+    bodyDamageClose: 31,
+    headDamageClose: 62,
+    bodyDamageMid: 31,
+    headDamageMid: 62,
+    bodyDamageLong: 31,
+    headDamageLong: 62,
+    bodyDamageMax: 31,
+    headDamageMax: 62,
+    impactDamageClose: 27,
+    impactDamageMid: 27,
+    impactDamageLong: 27,
+    impactDamageMax: 27,
+    environmentalDamageClose: 33,
+    environmentalDamageMid: 33,
+    environmentalDamageLong: 33,
+    environmentalDamageMax: 33,
+    baseSpread: .15,
+    sprintSpread: 1.5,
+    fallingJumpingSpread: 1.25,
+    adsSpread: .6,
+    standingSpread: .55,
+    crouchingSpread: .8,
+    horizontalRecoil: 0.25,
+    horizontalRecoilGamepad: 1,
+    verticalRecoil: 3.4125,
+    verticalRecoilGamepad: .5,
+    angleMax: 25,
+    angleMin: -25,
+    adsRecoil: .5,
+    interpSpeed: 12,
+    interpSpeedRecovery: 6,
+    fireRate: 5.5,
+    reloadTime: 2.2,
+    magazineSize: 30,
+    floorLootChance: .259,
+    chestLootChance: .259,
+    supplyDropLootChance: 0
+  },
+  function (err, small) {
+    if (err) console.log(err);
+    // saved!
+});
+
+// Create rare m4 weapon document in Database
+Weapon.create({
+    image: "/public/images/weapons/assault-rifle-m4.png",
+    name: "assault-rifle-m4",
+    type: "assault-rifle",
+    tier: "rare",
+    closeRange: 1,
+    midrange: 1,
+    longRange: 1,
+    maxRange: 250000,
+    damagePerSecond: 181.5,
+    bodyDamageClose: 33,
+    headDamageClose: 66,
+    bodyDamageMid: 33,
+    headDamageMid: 66,
+    bodyDamageLong: 33,
+    headDamageLong: 66,
+    bodyDamageMax: 33,
+    headDamageMax: 66,
+    impactDamageClose: 28,
+    impactDamageMid: 28,
+    impactDamageLong: 28,
+    impactDamageMax: 28,
+    environmentalDamageClose: 33,
+    environmentalDamageMid: 33,
+    environmentalDamageLong: 33,
+    environmentalDamageMax: 33,
+    baseSpread: .15,
+    sprintSpread: 1.5,
+    fallingJumpingSpread: 1.25,
+    adsSpread: .6,
+    standingSpread: .55,
+    crouchingSpread: .8,
+    horizontalRecoil: 0.25,
+    horizontalRecoilGamepad: 1,
+    verticalRecoil: 3.325,
+    verticalRecoilGamepad: .5,
+    angleMax: 25,
+    angleMin: -25,
+    adsRecoil: .5,
+    interpSpeed: 12,
+    interpSpeedRecovery: 6,
+    fireRate: 5.5,
+    reloadTime: 2.2,
+    magazineSize: 30,
+    floorLootChance: .104,
+    chestLootChance: .104,
+    supplyDropLootChance: 0
+  },
+  function (err, small) {
+    if (err) console.log(err);
+    // saved!
+});
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Fortnite Stats', active: 'home' });
+  res.render('pages/index', { title: 'Fortnite | Home', active: 'home' });
 });
 
 module.exports = router;
