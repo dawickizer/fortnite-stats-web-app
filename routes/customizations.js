d128a34c528bb851356bec1533cd30eaff40f4dd
EC2 Default User <ec2-user@ip-172-31-74-143.ec2.internal>
2018-05-07 18:34:12 +0000

    Added end points for all customizations pages

--- routes/customizations.js
+++ routes/customizations.js
@@ -6,34 +6,44 @@ router.get('/', function(req, res, next) {
   res.render('pages/customizations', {title: 'Fortnite | Customizations', active: 'customizations', style: 'customizations'});
 });
 
-/* GET skins route */
-router.get('/skins', function(req, res, next) {
-  res.send('This is the /customizations/skins page');
+/* GET outfits route */
+router.get('/outfits', function(req, res, next) {
+  res.render('pages/outfits', {title: 'Fortnite | Outfits', active: 'customizations', style: 'outfits'});
 });
 
-/* GET taunts route */
-router.get('/taunts', function(req, res, next) {
-  res.send('This is the /customizations/taunts page');
+/* GET emotes route */
+router.get('/emotes', function(req, res, next) {
+  res.render('pages/emotes', {title: 'Fortnite | Emotes', active: 'customizations', style: 'emotes'});
 });
 
-/* GET stickers route */
-router.get('/stickers', function(req, res, next) {
-  res.send('This is the /customizations/stickers page');
+/* GET emoticons route */
+router.get('/emoticons', function(req, res, next) {
+  res.render('pages/emoticons', {title: 'Fortnite | Emoticons', active: 'customizations', style: 'emoticons'});
 });
 
-/* GET pickaxes route */
-router.get('/pickaxes', function(req, res, next) {
-  res.send('This is the /customizations/pickaxes page');
+/* GET harvesting-tools route */
+router.get('/harvesting-tools', function(req, res, next) {
+  res.render('pages/harvesting-tools', {title: 'Fortnite | Harvesting Tools', active: 'customizations', style: 'harvesting-tools'});
 });
 
 /* GET gliders route */
 router.get('/gliders', function(req, res, next) {
-  res.send('This is the /customizations/gliders page');
+  res.render('pages/gliders', {title: 'Fortnite | Gliders', active: 'customizations', style: 'gliders'});
 });
 
-/* GET backpacks route */
-router.get('/backpacks', function(req, res, next) {
-  res.send('This is the /customizations/backpacks page');
+/* GET back-bling route */
+router.get('/back-bling', function(req, res, next) {
+  res.render('pages/back-bling', {title: 'Fortnite | Back Bling', active: 'customizations', style: 'back-bling'});
+});
+
+/* GET skydiving-trails route */
+router.get('/skydiving-trails', function(req, res, next) {
+  res.render('pages/skydiving-trails', {title: 'Fortnite | Skydiving Trails', active: 'customizations', style: 'skydiving-trails'});
+});
+
+/* GET loading-screens route */
+router.get('/loading-screens', function(req, res, next) {
+  res.render('pages/loading-screens', {title: 'Fortnite | Loading Screens', active: 'customizations', style: 'loading-screens'});
 });
 
 module.exports = router;
